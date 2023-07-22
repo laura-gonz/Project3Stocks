c24d9d80d80985ae001276314613a55d3565a0d9
perrymark773 <perrymark773@gmail.com>
2023-07-22 07:00:24 -0500

    Fleshing out data collection to SQLite database using pd to_sql initiated connection ot database in flask api

---
+++ flask_app.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+# Route to get data from the SQLite database
+@app.route('/crypto_data', methods=['GET'])
+def get_crypto_data():
+    try:
+        # Connect to the SQLite database
+        connection = sqlite3.connect('crypto_data.db')
+        cursor = connection.cursor()
+
+        # Execute a query to fetch data from the database
+        cursor.execute('SELECT * FROM crypto_data')
+
+        # Fetch all the rows from the query result
+        rows = cursor.fetchall()
+
+        # Close the database connection
+        connection.close()
+
+        # Convert the query result to a list of dictionaries
+        data = []
+        for row in rows:
+            data.append({
+                'timestamp': row[0],
+                'open': row[1],
+                'high': row[2],
+                'low': row[3],
+                'close': row[4],
+                'volume': row[5],
+                'ema_2': row[6],
+                'ema_10': row[7],
+                'macd': row[8],
+                'macd_signal': row[9],
+                'macd_histogram': row[10]
+            })
+
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
