fa2ce5843e727c9aa03f4a677eba9495fefea791
perrymark773 <perrymark773@gmail.com>
2023-07-24 21:05:58 -0500

    Working flask app, defined crypto names, mega list with all data, and endpoint for each different crypto

--- flask_app.py
+++ flask_app.py
@@ -1,56 +1,66 @@
-import numpy as np
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(autoload_with=engine)
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+import pandas as pd
 
 app = Flask(__name__)
+engine = create_engine('sqlite:///crypto_data.db')
+Session = sessionmaker(bind=engine)
+symbol_batch = ['BTC/USD', 'ETH/USD', 'USDT/USD', 'XRP/USD', 
+                'BNB/USD', 'ADA/USD', 'SOL/USD', 'DOGE/USD']
 
-# Route to get data from the SQLite database
-@app.route('/crypto_data', methods=['GET'])
-def get_crypto_data():
+@app.route("/")
+def welcome():
+    """List all available API routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/symbols<br/>"
+        f"/api/v1.0/cryptocurrencies<br/>"
+        f"/api/v1.0/cryptocurrencies/<symbol>"
+    )
+
+@app.route("/api/v1.0/symbols")
+def symbols():
     try:
-        # Connect to the SQLite database
-        connection = sqlite3.connect('crypto_data.db')
-        cursor = connection.cursor()
+        # Connect to the database
+        engine = create_engine("sqlite:///crypto_data.db")
+        conn = engine.connect()
 
-        # Execute a query to fetch data from the database
-        cursor.execute('SELECT * FROM crypto_data')
+        # Get the table names from the database
+        query = text("SELECT name FROM sqlite_master WHERE type='table';")
+        result = conn.execute(query).fetchall()
+        conn.close()
 
-        # Fetch all the rows from the query result
-        rows = cursor.fetchall()
+        # Extract the table names from the query result
+        tables = [row[0] for row in result]
 
-        # Close the database connection
-        connection.close()
+        return jsonify(tables)
 
-        # Convert the query result to a list of dictionaries
-        data = []
-        for row in rows:
-            data.append({
-                'timestamp': row[0],
-                'open': row[1],
-                'high': row[2],
-                'low': row[3],
-                'close': row[4],
-                'volume': row[5],
-                'ema_2': row[6],
-                'ema_10': row[7],
-                'macd': row[8],
-                'macd_signal': row[9],
-                'macd_histogram': row[10]
-            })
-
-        return jsonify(data)
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return f"Error: {str(e)}"
+
+@app.route("/api/v1.0/cryptocurrencies")
+def cryptocurrencies():
+    """Return all data for all cryptocurrencies."""
+    conn = engine.connect()
+    crypto_data = {}
+    for symbol in symbol_batch:
+        table_name = f'crypto_{symbol.replace("/", "_")}'
+        query = f"SELECT * FROM {table_name}"
+        df = pd.read_sql(query, conn)
+        crypto_data[symbol] = df.to_dict(orient='records')
+    conn.close()
+    return jsonify(crypto_data)
+
+@app.route("/api/v1.0/cryptocurrencies/<symbol>")
+def cryptocurrency(symbol):
+    """Return data for a specific cryptocurrency."""
+    conn = engine.connect()
+    table_name = f'crypto_{symbol.replace("/", "_")}'
+    query = f"SELECT * FROM {table_name}"
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df.to_json(orient='records')
 
 if __name__ == '__main__':
     app.run(debug=True)
